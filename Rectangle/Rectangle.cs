cc9c5c338bb491f4139c2abf4418ecfdf48e26d8
Jsidhu0642 <Jsidhu0642@conestogac.on.ca>
2020-02-02 10:40:31 -0500

    <commit message>

--- Rectangle/Rectangle.cs
+++ Rectangle/Rectangle.cs
@@ -4,22 +4,59 @@ using System.Text;
 
 namespace Rectangle
 {
-    class Rectangle
+    public class Rectangle
     {
+        // variable declare
         private int length;
         private int width;
-
+        // default construtor 
         public Rectangle() 
         {
             length = 1;
             width = 1;
             
         }
-        public Rectangle ( int length, int width)
+
+        // non- constructor 
+        public  Rectangle ( int length, int width)
         {
             this.length = length;
             this.width = width;
         }
 
+        //Six  Methods
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int SetLength(int length)
+        {
+            return this.length = length;
+        }
+
+
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int width)
+        {
+            return this.width = width;
+        }
+
+        public int GetPerimeter()
+        {
+            return GetPerimeter();
+        }
+
+
+        public int GetArea()
+        {
+            return GetArea();
+        }
     }
 }
