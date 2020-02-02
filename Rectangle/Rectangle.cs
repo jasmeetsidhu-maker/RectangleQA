09cbba04a975bbf72fd076f36e96616a44705cd1
Jsidhu0642 <Jsidhu0642@conestogac.on.ca>
2020-02-02 10:13:07 -0500

    <commit message>

---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle() 
+        {
+            length = 1;
+            width = 1;
+            
+        }
+        public Rectangle ( int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+    }
+}
