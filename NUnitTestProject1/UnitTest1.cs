f57187a6afa33861103c8cea6aa3222fcc27cdd1
Jsidhu0642 <Jsidhu0642@conestogac.on.ca>
2020-02-05 20:30:57 -0500

    containing the completed unit tests.

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -12,30 +12,132 @@ namespace Rectangle
         }
 
         [Test]
-        public void TestToGetIntValue()
+
+        // test for method Get Length 
+        public void CheckValueOfLengthIsNotZero()
         {
+            // arrange
             var rectangle = new Rectangle();
-            
 
-            //int result = rectangle.GetLength();
-
-            Assert.AreEqual( 4 , rectangle.GetLength());
+            // act 
+            int expectValue = 0;
+            // assert 
+            Assert.AreEqual(expectValue, rectangle.GetLength());
         }
-        [Test]
 
-        public void TestToSetValue()
+        // 1st test for method Set length 
+        [TestCase]
+         
+        public void CheckNegativeValueIsNotPassed()
         {
-
+            // Arrange
             var rectangle = new Rectangle();
-            
 
+            // Act 
+            int expectValue = -09;
+          
+
+            // Assert 
+            Assert.AreEqual(expectValue, rectangle.SetLength(1));
+        }
+
+
+        // 2nd test for method Set length
+
+        [TestCase]
+
+        public void AddedIntValueToSetLength()
+        {
+            // Arrange
+            var rectangle = new Rectangle();
+
+
+            // Act
             int result = rectangle.SetLength(5);
 
+            //Assert
             Assert.AreEqual(5, result);
         }
 
 
+        // test for method Get Width
+        [Test]
+        public void CheckValueOfWidthIsNotZero()
+        {
+            // Arrange
+            var Program = new Rectangle();
+
+            // act 
+            int expectValue = 0;
+            // assert 
+            Assert.AreEqual(expectValue, Program.GetWidth());
+        }
 
 
+        // 1st test for method Set width
+        public void NegativeValueIsNotPassed()
+        {
+            // Arrange
+
+            var Program = new Rectangle();
+
+            // Act 
+            int expectValue = -77;
+
+            // Assert 
+            Assert.AreEqual(expectValue, Program.SetWidth(1));
+        }
+
+        // 2nd test for method Set width
+
+        [TestCase]
+
+        public void AddedIntValueToSetWidth()
+        {
+            // Arrange
+            var rectangle = new Rectangle();
+
+
+            // Act
+            int result = rectangle.SetWidth(55);
+
+            //Assert
+            Assert.AreEqual(55, result);
+        }
+
+
+        
+
+
+        //  test for method GetArea()
+        [TestCase]
+
+        public void ValueToGetArea()
+        {
+            // Arrange
+            var rectangle = new Rectangle();
+
+            // Act
+            int expectValue =   3 * 3;
+            
+            //Assert
+            Assert.AreEqual(expectValue, rectangle.GetArea());
+        }
+
+        //  test for method GetPerimeter()
+
+        [TestCase]
+
+        public void AddedIntValueToGetPerimeter()
+        {
+            // Arrange
+            var rectangle = new Rectangle();
+
+            // Act
+
+            int expectValue = 55;
+            //Assert
+            Assert.AreEqual(expectValue, rectangle.GetPerimeter());
+        }
     }
 }
